625a2bec5825c81b7499b42c1686165b70428436
Mrazo04 <munguiamickey@gmail.com>
2023-03-10 14:57:11 -0600

    added filter button and filter by userId function

--- todo.js
+++ todo.js
@@ -1,48 +1,80 @@
 let arrayOfTodos = [
-    {
-    "userId": 14,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-},
-{
-    "userId": 20,
-    "id": 2,
-    "title": "delectus aut autem",
-    "completed": false
-}]
+  {
+    userId: 14,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  },
+  {
+    userId: 20,
+    id: 2,
+    title: "delectus aut autem",
+    completed: false,
+  },
+];
 
-   console.log(arrayOfTodos[0].userId) // => 14
-    console.log(arrayOfTodos[1].userId) // => 20
-
+console.log(arrayOfTodos[0].userId); // => 14
+console.log(arrayOfTodos[1].userId); // => 20
 
 const fetchTodos = () => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-    .then( (response) => response.json())
-    .then( (json) => arrayOfTodos = json)
-}
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.json())
+    .then((json) => (arrayOfTodos = json));
+};
 
 const logTodos = () => {
-    console.log(arrayOfTodos)
-}
+  console.log(arrayOfTodos);
+};
 
 const populateTodos = () => {
-    let ol = document.getElementById("todo-list")
-// get OL element from HTML page
-for (let index = 0; index < arrayOfTodos.length; index++) {
+  let ol = document.getElementById("todo-list");
+  // get OL element from HTML page
+  for (let index = 0; index < arrayOfTodos.length; index++) {
     const todo = arrayOfTodos[index];
     const li = document.createElement("li");
     const title = document.createTextNode(todo.title);
     li.appendChild(title);
     ol.appendChild(li);
 
-    console.log(arrayOfTodos[index])
-}
+    console.log(arrayOfTodos[index]);
+  }
+  // loop thru arrayOfTodos
+  // individual todo item in arrayOfTodos array
+  //create LI element
+  //create text using the title of each obj in the array
+  // append text to LI element
+  // append LI element to  OL element
+};
 
-    // loop thru arrayOfTodos
-      // individual todo item in arrayOfTodos array
-      //create LI element
-      //create text using the title of each obj in the array
-      // append text to LI element
-      // append LI element to  OL element
+let storedFilter = []
+
+const filterTodo = () => {
+    let ol = document.getElementById("todo-list");
+    ol.innerHTML= ""
+  let userId = document.getElementById("userId").value;
+  let filterArray = arrayOfTodos.filter((todo) => todo.userId == userId);
+
+  for (let index = 0; index < filterArray.length; index++) {
+    const todo = filterArray[index];
+    const li = document.createElement("li");
+    const title = document.createTextNode(todo.title);
+    li.appendChild(title);
+    ol.appendChild(li);
+    storedFilter.push(todo)
+    console.log(filterArray[index]);
+  }
+};
+
+const completedTodos = () => {
+    for (let index = 0; index < storedFilter.length; index++) {
+        const todo = filterArray[index];
+        if(todo.completed == true){
+            const li = document.createElement("li");
+            const title = document.createTextNode(todo.title);
+            li.appendChild(title);
+            ol.appendChild(li);
+            storedFilter.push(todo)
+            console.log(filterArray[index]);
+        }
+}
 }
